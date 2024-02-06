fb7adff23229b29912842d5a7c7d4d6ee52ea08d
rashi9551 <rashidroz1361@gmail.com>
2024-02-06 11:36:10 +0530

    stating new journe y

---
+++ sample.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var word = "Welcome Back";
+console.log(word);
---
+++ sample.ts
@@ -0,0 +1,3 @@
+export {}
+let word="Welcome Back"
+console.log(word);
